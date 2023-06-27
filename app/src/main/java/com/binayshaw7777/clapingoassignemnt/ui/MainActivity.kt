217b79aa225cc76ebe7c52de160acac031dec16a
Binay Shaw <binayshaw7777@gmail.com>
2023-06-28 01:31:09 +0530

    Added DateTime cards UI

--- app/src/main/java/com/binayshaw7777/clapingoassignemnt/ui/MainActivity.kt
+++ app/src/main/java/com/binayshaw7777/clapingoassignemnt/ui/MainActivity.kt
@@ -1,23 +1,22 @@
 package com.binayshaw7777.clapingoassignemnt.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.lifecycle.ViewModelProvider
 import com.binayshaw7777.clapingoassignemnt.R
+import com.binayshaw7777.clapingoassignemnt.R.id.*
 import com.binayshaw7777.clapingoassignemnt.databinding.ActivityMainBinding
 import com.binayshaw7777.clapingoassignemnt.model.ApiRequest
 import com.binayshaw7777.clapingoassignemnt.model.Teacher
 import com.binayshaw7777.clapingoassignemnt.model.Timeslot
 import com.binayshaw7777.clapingoassignemnt.utils.Logger
-import com.binayshaw7777.clapingoassignemnt.utils.hide
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -57,7 +56,7 @@ class MainActivity : AppCompatActivity() {
         apiRequest.apply {
             if (statusCode != 200) return
 
-            this.teacher?.let { updateTeacherDetails(it) }
+            this.teacher?.let { updateTeacherDetails(it, apiRequest.rating ?: 0.0) }
             if (bookedTimings != null && timeslot != null) {
                 updateTimeSlots(bookedTimings!!, timeslot!!)
             }
@@ -68,11 +67,12 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun updateTeacherDetails(teacher: Teacher) {
+    private fun updateTeacherDetails(teacher: Teacher, rating: Double) {
         binding.teacherNameTextView.text = teacher.name.toString()
         Logger.debugLog("Teacher name is: ${teacher.name}")
         Glide.with(this@MainActivity).load(teacher.image).diskCacheStrategy(
             DiskCacheStrategy.DATA
         ).into(binding.teacherImageView)
+        binding.ratingIncludeLayout.ratingTextView.text = rating.toString()
     }
 }
